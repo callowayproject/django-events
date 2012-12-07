a5d84859d32fb515b22612cfe39e3c3dd95f94ce
Corey Oordt <coreyoordt@gmail.com>
2012-12-07 06:57:10 -0600

    Adding generic collections into the admin

--- events/admin.py
+++ events/admin.py
@@ -1,19 +1,29 @@
 from django.contrib import admin
 
-from events.models import Calendar, Event, CalendarRelation, Rule
+from .genericcollection import GenericCollectionTabularInline
+from .models import Calendar, Event, CalendarRelation, EventRelation, Rule
+
+
+class CalendarRelationInline(GenericCollectionTabularInline):
+    model = CalendarRelation
 
 
 class CalendarAdminOptions(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
     search_fields = ['name']
+    inlines = [CalendarRelationInline]
+
+
+class EventRelationInline(GenericCollectionTabularInline):
+    model = EventRelation
 
 
 class EventAdmin(admin.ModelAdmin):
     search_fields = ('title', )
     list_display = ('title', 'start', 'end', 'all_day', 'rule')
     list_filter = ('start', 'end', 'all_day', 'rule')
+    inlines = [EventRelationInline]
+
 admin.site.register(Event, EventAdmin)
-
-
 admin.site.register(Calendar, CalendarAdminOptions)
 admin.site.register([Rule, CalendarRelation])
