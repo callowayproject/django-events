e4d87a8e51f31d438dbb5a5015dc931d5b475e47
Corey Oordt <coreyoordt@gmail.com>
2013-05-16 12:37:21 -0500

    Made the Rule have its own Admin

--- events/admin.py
+++ events/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .genericcollection import GenericCollectionTabularInline
 from .models import Calendar, Event, CalendarRelation, EventRelation, Rule
+from .forms import RuleForm
 
 
 class CalendarRelationInline(GenericCollectionTabularInline):
@@ -24,6 +25,11 @@ class EventAdmin(admin.ModelAdmin):
     list_filter = ('start', 'end', 'all_day', 'rule')
     inlines = [EventRelationInline]
 
+
+class RuleAdmin(admin.ModelAdmin):
+    form = RuleForm
+
+
+admin.site.register(Rule, RuleAdmin)
 admin.site.register(Event, EventAdmin)
 admin.site.register(Calendar, CalendarAdminOptions)
-admin.site.register([Rule, CalendarRelation])
